7cf24a50921f8cb31a255082f9bc97a633015217
Nidhisharma2910 <nidhi2112sharma@gmail.com>
2023-10-31 22:28:49 +0530

    adding main page

---
+++ app/src/main/java/com/example/medicare/AppointmentActivity.kt
@@ -0,0 +1,17 @@
+package com.example.medicare
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class AppointmentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appointment)
+
+
+    }
+
+}
--- app/src/main/java/com/example/medicare/LoginActivity.kt
+++ app/src/main/java/com/example/medicare/LoginActivity.kt
@@ -1,14 +1,26 @@
 package com.example.medicare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val appointment = findViewById<Button>(R.id.button)
+        appointment.setOnClickListener {
+            val intent = Intent(this, AppointmentActivity::class.java)
+            startActivity(intent)
+        }
+
+
 
     }
 
