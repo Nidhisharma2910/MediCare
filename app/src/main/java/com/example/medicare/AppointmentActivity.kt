8a736eeb77c8f90ffb1b1e51d392cbd8c58cd50e
Nidhisharma2910 <nidhi2112sharma@gmail.com>
2023-11-01 08:40:44 +0530

    second Activity

--- app/src/main/java/com/example/medicare/AppointmentActivity.kt
+++ app/src/main/java/com/example/medicare/AppointmentActivity.kt
@@ -2,14 +2,21 @@ package com.example.medicare
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 class AppointmentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_appointment)
-    }
 
+        val second = findViewById<TextView>(R.id.see)
+        second.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
 }
 
 
